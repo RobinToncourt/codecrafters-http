69dcfea261defb1d425e47e3a53be553adc49d23
Robin TONCOURT <robin.toncourt@gmail.com>
2024-09-14 14:02:31 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -6,12 +7,34 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let response: String = generate_response();
+                stream.write(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn generate_response() -> String {
+    let status = generate_status_line();
+    let header = generate_header();
+    let content = generate_body();
+
+    format!("{status}{header}{content}")
+}
+
+fn generate_status_line() -> String {
+    format!("HTTP/1.1 200 OK\r\n")
+}
+
+fn generate_header() -> String {
+    format!("\r\n")
+}
+
+fn generate_body() -> String {
+    format!("")
 }
