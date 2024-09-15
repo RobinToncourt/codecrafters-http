d399c1bd5121e104797fb92fcc40aec53ec810f1
Robin TONCOURT <robin.toncourt@gmail.com>
2024-09-15 21:32:00 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 #[allow(unused_imports)]
 use std::fmt;
+use std::collections::HashMap;
 use std::io::{
     prelude::*,
     Write,
@@ -18,30 +19,44 @@ enum HttpError {
 
 }
 
-#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 struct HttpRequest {
     request_line: RequestLine,
-    headers: Vec<Header>,
+    headers: HashMap<String, String>,
     request_body: RequestBody,
 }
 
 impl HttpRequest {
     fn from_stream(stream: &mut TcpStream) -> Result<Self, HttpError> {
         let buf_reader = BufReader::new(stream);
-        let mut http_request: Vec<String> = buf_reader
+        let http_request: Vec<String> = buf_reader
             .lines()
             .map(|result| result.unwrap())
             .take_while(|line| !line.is_empty())
             .collect();
 
-        let request_body = RequestBody(http_request.pop().unwrap());
-        let mut headers: Vec<Header> = Vec::new();
-        while http_request.len() > 1 {
-            headers.push(Header::from_str(&http_request.pop().unwrap()).unwrap())
-        }
-        headers.reverse();
         let request_line =
-            RequestLine::from_str(&http_request.pop().unwrap()).unwrap();
+            RequestLine::from_str(&http_request[0]).unwrap();
+
+        let mut headers: HashMap<String, String> = HashMap::new();
+        let mut index: usize = 1;
+        loop {
+            let Some(tmp) = &http_request.get(index) else {
+                break;
+            };
+            let Some((key, value)) = Header::parse(tmp) else {
+                break;
+            };
+            headers.insert(key, value);
+            index += 1;
+        }
+        let request_body =
+            RequestBody(if let Some(tmp) = &http_request.get(index) {
+            tmp
+        }
+        else {
+            ""
+        }.to_string());
 
         Ok(Self { request_line, headers, request_body })
     }
@@ -67,32 +82,6 @@ impl RequestLine {
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
-struct Header {
-    name: String,
-    value: String,
-}
-
-impl Header {
-    fn from_str(header: &str) -> Result<Self, HttpError> {
-        let mut split = header.split(": ");
-
-        let name = split.next().unwrap().to_string();
-        let value = split.next().unwrap().to_string();
-
-        Ok(Self { name, value })
-    }
-}
-
-impl fmt::Display for Header {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}: {}{CRLF}",
-               self.name,
-               self.value,
-        )
-    }
-}
-
-#[derive(PartialEq, Eq, Hash, Clone, Debug)]
 struct RequestBody(String);
 
 impl fmt::Display for RequestBody {
@@ -111,9 +100,9 @@ struct HttpResponse {
 impl fmt::Display for HttpResponse {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}{CRLF}{}{CRLF}{}",
-            self.status_line,
-            join_headers(&self.headers),
-            self.response_body
+               self.status_line,
+               join_headers(&self.headers),
+               self.response_body
         )
     }
 }
@@ -122,9 +111,9 @@ fn join_headers(headers: &Vec<Header>) -> String {
     let mut joined_headers = String::new();
 
     headers.iter()
-        .for_each(
-            |header| joined_headers.push_str(&header.to_string())
-        );
+    .for_each(
+        |header| joined_headers.push_str(&header.to_string())
+    );
 
     joined_headers
 }
@@ -151,9 +140,9 @@ impl StatusLine {
 impl fmt::Display for StatusLine {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} {} {}",
-            self.http_version,
-            self.status_code as usize,
-            self.status_code.reason_phrase()
+               self.http_version,
+               self.status_code as usize,
+               self.status_code.reason_phrase()
         )
     }
 }
@@ -170,6 +159,40 @@ impl StatusCode {
             StatusCode::OK => "OK",
             StatusCode::NotFound => "Not Found",
         }.to_string()
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+struct Header {
+    name: String,
+    value: String,
+}
+
+impl Header {
+    fn new(name: String, value: String) -> Self {
+        Self { name, value }
+    }
+
+    fn parse(header: &str) -> Option<(String, String)> {
+        let mut split = header.split(": ");
+
+        let Some(name) = split.next() else {
+            return None;
+        };
+        let Some(value) = split.next() else {
+            return None;
+        };
+
+        Some((name.to_string(), value.to_string()))
+    }
+}
+
+impl fmt::Display for Header {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: {}{CRLF}",
+               self.name,
+               self.value,
+        )
     }
 }
 
@@ -200,19 +223,27 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let http_request = HttpRequest::from_stream(&mut stream).unwrap();
 
-    let response: String =
-        if http_request.request_line.request_target.eq("/") {
+    let request_target: &str = &http_request.request_line.request_target;
+    let response: String = if request_target.eq("/") {
         format!("HTTP/1.1 200 OK{CRLF}{CRLF}")
     }
-    else if http_request.request_line.request_target.starts_with("/echo/") {
-        let (mut headers, response_body): (Vec<Header>, ResponseBody) =
-            echo_page(&http_request);
+    else if request_target.starts_with("/echo/") {
+        let (headers, response_body): (Vec<Header>, ResponseBody) =
+        echo_page(&http_request);
 
-        let content_type = Header {
-            name: "Content-Type".to_string(),
-            value: "text/plain".to_string(),
+        let status_line = StatusLine::new(StatusCode::OK);
+
+        let http_response = HttpResponse {
+            status_line,
+            headers,
+            response_body,
         };
-        headers.insert(0, content_type);
+
+        format!("{http_response}")
+    }
+    else if request_target.eq("/user-agent") {
+        let (headers, response_body): (Vec<Header>, ResponseBody) =
+        user_agent_page(&http_request);
 
         let status_line = StatusLine::new(StatusCode::OK);
 
@@ -228,25 +259,50 @@ fn handle_connection(mut stream: TcpStream) {
         format!("HTTP/1.1 404 Not Found{CRLF}{CRLF}")
     };
 
+    println!("{response}");
     stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn echo_page(http_request: &HttpRequest) -> (Vec<Header>, ResponseBody) {
-    let split: Vec<&str> =
-        http_request.request_line.request_target.split("/").collect();
+    let split: Vec<&str> = http_request
+    .request_line
+    .request_target
+    .split("/")
+    .collect();
 
     let response_body = ResponseBody(split[split.len()-1].to_string());
 
-    let content_length = Header {
-        name: "Content-Length".to_string(),
-        value: response_body.0.len().to_string(),
-    };
-    let headers: Vec<Header> = vec![content_length];
+    let content_type = Header::new(
+        "Content-Type".to_string(),
+                                   "text/plain".to_string(),
+    );
+    let content_length = Header::new(
+        "Content-Length".to_string(),
+                                     response_body.0.len().to_string(),
+    );
+    let headers: Vec<Header> = vec![content_type, content_length];
 
     (headers, response_body)
 }
 
+fn user_agent_page(http_request: &HttpRequest) -> (Vec<Header>, ResponseBody) {
+    let user_agent_value: &str =
+    http_request.headers.get("User-Agent").expect("No User-Agent header");
 
+    let response_body = ResponseBody(user_agent_value.to_string());
+
+    let content_type = Header::new(
+        "Content-Type".to_string(),
+                                   "text/plain".to_string(),
+    );
+    let content_length = Header::new(
+        "Content-Length".to_string(),
+                                     response_body.0.len().to_string(),
+    );
+    let headers: Vec<Header> = vec![content_type, content_length];
+
+    (headers, response_body)
+}
 
 
 
